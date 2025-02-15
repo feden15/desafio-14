1c6c1ac87af194cba8ffdc0f2bc5782e14faf17b
Federico Nova <fedenova8@gmail.com>
2025-02-15 13:09:40 -0300

    Estilizamos con Tailwind y correctamente, UserCard.jsx

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,8 +1,13 @@
 // rafce
 
-const UserCard = () => {
+const UserCard = ({ nombre, edad, profesion }) => {
   return (
-    <div>UserCard</div>
+    <div className="bg-blue-100 p-4 rounded-lg shadow-md mt-2 w-max m-auto">
+      <h2 className="text-xl text-center font-bold">{nombre}</h2>
+      <hr className="mb-1"/>
+      <p>Edad: {edad}</p>
+      <p>Profesión: {profesion}</p>
+    </div>
   )
 }
 
