583146d16639e94ddaaa6c82c159e5abb8e26ea4
Federico Nova <fedenova8@gmail.com>
2025-02-15 13:52:55 -0300

    Finalizamos de crear y estilizar, LikeButton.jsx

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react"
+
 const LikeButton = () => {
+    const [liked, setLiked] = useState(true)
+
   return (
-    <div>LikeButton</div>
+    <div className="flex justify-center">
+        <button
+            onClick={() => setLiked(!liked)}
+            className="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded-lg cursor-pointer"
+        >
+            {liked ? "Like" : "Unlike"}
+        </button>
+    </div>
   )
 }
 
